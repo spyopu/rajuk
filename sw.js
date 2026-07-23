e0e335ca06bb139f45316b588f597f86d8e6b977
Dreamland Design <designkhoor@gmail.com>
2026-07-23 06:14:19 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'digital-khata-v1.1';
+const CACHE_NAME = 'digital-khata-v3';
 const assetsToCache = [
   '/rajuk/',
   '/rajuk/index.html',
@@ -6,18 +6,42 @@ const assetsToCache = [
   'https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@400;500;600;700&family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap'
 ];
 
+// Install Event: ক্যাশ ফাইলগুলো স্টোর করা
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(assetsToCache);
     })
   );
+  self.skipWaiting();
 });
 
+// Activate Event: পুরানো ক্যাশ ক্লিনআপ করা
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch Event: আইকন বা সরাসরি অ্যাপ ওপেন করলে 404 রোধ করা
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).catch(() => {
+        return caches.match('/rajuk/index.html');
+      });
     })
   );
 });
